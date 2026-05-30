1a8884bf0eb375da123d72d4d14edd23868c9d37
Nadya <studenikina.nadya@gmail.com>
2026-05-30 15:08:20 +0200

    First commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,10 @@
+import { fetchNotes } from "../../lib/api";
+import NotePage from "./NotesPage";
+
+const Notes = async () => {
+  const notes = await fetchNotes("", 1);
+
+  return <NotePage res={notes} />;
+};
+
+export default Notes;
